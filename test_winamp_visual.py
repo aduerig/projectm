5cc3c2158ff80199372aaf7bb370fc9d116f79ba
Andrew Duerig <duerig.andrew@gmail.com>
2023-10-25 16:35:28 -0400

    ok

--- test_winamp_visual.py
+++ test_winamp_visual.py
@@ -16,7 +16,7 @@
     # PARTIAL BUILD:
         # rm winamp_visual.cpython-311-x86_64-linux-gnu.so; python build_c_module_for_python.py build --build-lib=. && LD_LIBRARY_PATH=src/libprojectM:/home/pi/random/sdl_install/SDL-release-2.28.4/build/.libs/ python test_winamp_visual.py
     # FULL BUILD:
-        # rm CMakeCache.txt; cmake -DCMAKE_BUILD_TYPE=Release && cmake --build . -- -j3 && rm winamp_visual.cpython-311-x86_64-linux-gnu.so; python build_c_module_for_python.py build --build-lib=. && LD_LIBRARY_PATH=src/libprojectM:/home/pi/random/sdl_install/SDL-release-2.28.4/build/.libs/ python test_winamp_visual.py
+        # rm CMakeCache.txt; cmake -DCMAKE_BUILD_TYPE=Release && cmake --build . -- -j4 && rm winamp_visual.cpython-311-x86_64-linux-gnu.so; python build_c_module_for_python.py build --build-lib=. && LD_LIBRARY_PATH=src/libprojectM:/home/pi/random/sdl_install/SDL-release-2.28.4/build/.libs/ python test_winamp_visual.py
 
 
 
