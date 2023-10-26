90f928fa85161ec0153ac504d073cdff3205bf14
Andrew Duerig <duerig.andrew@gmail.com>
2023-10-25 22:27:52 -0400

    ok

--- test_winamp_visual.py
+++ test_winamp_visual.py
@@ -88,7 +88,7 @@ def start_listen_keys():
 
         threading.Thread(target=listen_for_keystrokes, args=[], daemon=True).start()
     elif is_doorbell():
-        print_red('NOT LISTENING FOR KEYSTROKES, NEED TO IMPLEMENT THIS')
+        # print_red('NOT LISTENING FOR KEYSTROKES, NEED TO IMPLEMENT THIS')
         from sshkeyboard import listen_keyboard
         def press(key):
             print(f'ssh keyboard detected key: {key}')
